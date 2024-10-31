4603ee46659cbcc925c772d0f862c03fccb0e00c
nurik-fire <abdinurik93@gmail.com>
2024-10-31 18:19:23 +0300

    int

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+let name ='Ivan'
+console.log(name);
+ 
+return (
+  <div>
+    <h1>Привет мир</h1>
+    <h2>Привет {name}</h2>
+  </div>
+)
+
+}
+
+export default App
